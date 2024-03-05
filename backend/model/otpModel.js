17fa3597a262326655c6e1342989163d57266bd9
unknown <Administrator@CWLAPTOP03.abacus.com>
2024-03-05 18:19:22 +0530

    hello

--- backend/model/otpModel.js
+++ backend/model/otpModel.js
@@ -38,7 +38,7 @@ otpSchema.pre("save", async function (next) {
   // Only send an email when a new document is created
   if (this.isNew) {
     await sendVerificationEmail(this.email, this.otp);
-  }
+  }  
   next();
 });
 module.exports = mongoose.model("OTP", otpSchema);
